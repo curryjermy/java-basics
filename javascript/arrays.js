6b22589d380703b06165819f0d4d01878b544009
Khari <curryjermy@gmail.com>
2024-07-08 08:52:10 -0700

    files added

---
+++ javascript/arrays.js
@@ -0,0 +1,13 @@
+let name = "khari";
+let age = 21;
+console.log(age);
+const cars = ["mercedes", "toyota", 2, 6];
+cars[4]="jeremy"
+console.log(cars.length);
+console.log("paper");
+
+//function greeting my mom
+function greeting(name, lastName){
+    console.log("hello "+name+ ' ' + lastName);   
+}
+greeting("mom","love");
