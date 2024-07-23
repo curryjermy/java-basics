ba0f7636392d6a4c1a1cc5e175cd6b44d924ef28
Khari <curryjermy@gmail.com>
2024-07-23 16:59:30 -0700

    comments added and removed as well

--- javascript/highOrder.js
+++ javascript/highOrder.js
@@ -1,69 +1,81 @@
 
-// // let addition = a + b;
-// // //declaration function
-// // function addNumbers(a, b) {
-// //     return addition;
-// // }
-// // //arrow function
-// // const addNumbers = (a, b) => {
-// //     return addition;
-// // }
-// // // expression function
-// // const addNumbers = function (a, b) {
-// //     return addition;
-// // }
-// // //anonymous function(no name used inside eventListeners and other functions)
-// // setTimeout(function (a, b) {
-// //     return addition;
-// // }, 400)
-// //     //immediately evoked function
-// //     (function (a, b) {
-// //         return addition;
-// //     })()
-// // //callback function(passed as a peremeter in another function)
-// // let callback = function (n) {
-// //     return n ** 2;
-// // };
-// // function cubed(callback, n) {
-// //     return callback(n) * n;
-// // };
-// // console.log(cubed(callback, 4));
+let addition = a + b;
 
-// // function greet (name,callback){
-// //     console.log("hello " + name);
-// //     callback ();
-// // }
-// // function sayGoodBye(){
-// // console.log("goodbye")
-// // }
-// // greet("Khari",sayGoodBye);
+//declaration function
+function addNumbers(a, b) {
+    return addition;
+}
 
-// function fetchData(callback) {
-//     setTimeout(function () {
-//         console.log("Data fetched successfully");
-//         callback("Data");
-//     }, 20000);
-// };
-// function displayData(data) {
-//     console.log("processing data..." + data);
-// }
-// fetchData(displayData);
+//arrow function
+const addNumbers = (a, b) => {
+    return addition;
+}
+
+// expression function
+const addNumbers = function (a, b) {
+    return addition;
+}
+
+//anonymous function(no name used inside eventListeners and other functions)
+setTimeout(function (a, b) {
+    return addition;
+}, 400)
+    //immediately evoked function
+    (function (a, b) {
+        return addition;
+    })()
+
+//callback function(passed as a peremeter in another function)
+//example
+let callback = function (n) {
+    return n ** 2;
+};
+function cubed(callback, n) {
+    return callback(n) * n;
+};
+console.log(cubed(callback, 4));
 
 
 
-//inbuilt high order function (for each)
-// let sum = 0;
-// let numbers = [1, 2, 3, 4, 5];
-// numbers.forEach(function(n){
-// return(sum=sum+n);
-// })
-// console.log(sum);
+//example
+function greet (name,callback){
+    console.log("hello " + name);
+    callback ();
+}
+function sayGoodBye(){
+console.log("goodbye")
+}
+greet("Khari",sayGoodBye);
 
 
-// let grains=["maize","gnuts","rice","beans"];
-// grains.forEach(function(grain){
-//     console.log(grain.toUpperCase());
-// })
+
+//example
+function fetchData(callback) {
+    setTimeout(function () {
+        console.log("Data fetched successfully");
+        callback("Data");
+    }, 20000);
+};
+function displayData(data) {
+    console.log("processing data..." + data);
+}
+fetchData(displayData);
+
+
+
+// inbuilt high order function (for each)
+let sum = 0;
+let numbersplain = [1, 2, 3, 4, 5];
+numbersplain.forEach(function(n){
+return(sum=sum+n);
+})
+console.log(sum);
+
+
+let grains=["maize","gnuts","rice","beans"];
+grains.forEach(function(grain){
+    console.log(grain.toUpperCase());
+})
 
 //map function (also takes in a callback)
 
